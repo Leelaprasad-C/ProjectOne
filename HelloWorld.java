6635d47a71c6dfb3ed796659737cba1b56fe781a
Leelaprasad C <leelaprasad.java@gmail.com>
2024-11-14 01:43:40 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -9,15 +9,28 @@ public class HelloWorld {
         this.j = j;
     }
 
+    public HelloWorld() {
+
+    }
+
     public int addSum() {
+        return i + j;
+
+    }
+
+    public int addSum(int i, int j) {
         return i + j;
 
     }
 
     public static void main(String args[]) {
         System.out.println("HelloWorld!");
-        HelloWorld hw = new HelloWorld(10, 20);
-        System.out.println("sum : " + hw.addSum());
+        HelloWorld hw1 = new HelloWorld(10, 20);
+        HelloWorld hw2 = new HelloWorld();
+
+        System.out.println("Sum Case 1 : " + hw1.addSum());
+        System.out.println("sum Case 2 : " + hw2.addSum(20, 30));
+
     }
 
 }
