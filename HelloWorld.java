99dbab67bb77b96f5d34a52828f72e806891825a
Leelaprasad C <leelaprasad.java@gmail.com>
2024-11-14 00:36:52 +0530

    HelloWorld working

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,23 @@
-pulblic class HelloWorld{
-  public static void main(){
-    System.out.println("HelloWorld!);
-  }  
+
+public class HelloWorld {
+
+    int i;
+    int j;
+
+    public HelloWorld(int i, int j) {
+        this.i = i;
+        this.j = j;
+    }
+
+    public int addSum() {
+        return i + j;
+
+    }
+
+    public static void main(String args[]) {
+        System.out.println("HelloWorld!");
+        HelloWorld hw = new HelloWorld(10, 20);
+        System.out.println("sum : " + hw.addSum());
+    }
+
 }
